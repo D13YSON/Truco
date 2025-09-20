c886896201588403760182a9c88ef8048cb29290
D13YSON <dieysonjason@gmail.com>
2025-09-20 16:42:34 -0300

    Initial commit

---
+++ src/main/java/dev/com/br/App.java
@@ -0,0 +1,13 @@
+package dev.com.br;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
